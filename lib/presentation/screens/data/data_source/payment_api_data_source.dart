951629563d7d4a3705840c39f5b80527469b5633
ZiyadSobhy <ziad60189@gmail.com>
2026-09-15 17:42:02 +0300

    merge into development

--- lib/presentation/screens/data/data_source/payment_api_data_source.dart
+++ lib/presentation/screens/data/data_source/payment_api_data_source.dart
@@ -10,9 +10,9 @@ class PaymentApiDataSource implements PaymentRemoteDataSource {
   static const String _baseUrl = 'https://accept.paymob.com';
   static const String _checkoutUrl = 'https://eg.checkout.paymob.com/';
   static const String _secretKey =
-      "egy_sk_test_783af2f6dc06e3d02b47613838ce0e5cd83d7fe72ddecb78e382452c2beb676a";
+      "..............................";
   static const String _publicKey =
-      "egy_pk_test_sp6OwB0hYsVp5fAfx03f9DpXQx9JUGS1";
+      "...................................................";
 
   @override
   Future<Map<String, dynamic>> createIntention({
